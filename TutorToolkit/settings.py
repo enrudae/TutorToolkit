a67ccf728734987de3d27c7ac3154ee2f622b0db
dmitry <malyutin994@gmail.com>
2024-05-02 22:36:31 +0500

    s3 file storage is connected

--- TutorToolkit/settings.py
+++ TutorToolkit/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'storages',
     'corsheaders',
     'rest_framework',
     'django_celery_beat',
@@ -213,6 +214,17 @@ CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL')
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_BROKER_URL')
 CELERY_IMPORTS = ['apps.notifications.tasks']
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+
+# ----Yandex s3----
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+YANDEX_CLIENT_DOCS_BUCKET_NAME = os.environ.get('YANDEX_CLIENT_DOCS_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('YANDEX_CLIENT_DOCS_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+
+AWS_S3_ENDPOINT_URL = 'https://storage.yandexcloud.net'
+AWS_S3_REGION_NAME = 'storage'
 
 
 # LOGGING = {
